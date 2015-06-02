62940543cd5cc2ced43c7dc9ab2944cd20374d69
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-06-02 09:07:09 -0400

    updated events, include render in callback

--- js/src/events.js
+++ js/src/events.js
@@ -1,17 +1,34 @@
 (function(){
-    var events = ['click', 'mouseover', 'mousemove', 'mouseenter', 'mouseleave'];
+    var events = ['click', 'mouseover', 'mousemove', 'mouseenter', 'mouseleave', 'mousedown', 'mouseup'];
 
     events.forEach(function(evt){
         Martin.prototype[evt] = function(cb) {
-            this.canvas.addEventListener(evt, cb);
+
+            var canvas = this;
+
+            function callback(e) {
+                cb(e);
+                canvas.render();
+            }
+
+            this.canvas.addEventListener(evt, callback);
             return this;
         };
     });
 
     Martin.prototype.on = function(evt, cb) {
-        if ( events.indexOf(evt) > -1 ) {
-            this.canvas.addEventListener(evt, cb);
+
+        var canvas = this;
+
+        function callback(e) {
+            cb(e);
+            canvas.render();
         }
+
+        if ( events.indexOf(evt) > -1 ) {
+            this.canvas.addEventListener(evt, callback);
+        }
+        
         return this;
     };
 })();
