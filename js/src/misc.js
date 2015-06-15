899963bafab4ffcc80d684064325009d58faa128
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-06-15 09:52:08 -0400

    updates to .width() / .height()

--- js/src/misc.js
+++ js/src/misc.js
@@ -10,37 +10,27 @@
 
 	Martin.prototype[which] = function( val, resize ) {
 
+		var ratio;
+
 		// if no value given, return the corresponding value
 		if ( !val ) return this.canvas[which];
-
-		var imageData,
-			pixels,
-			oldHeight,
-			ratio,
-			dummyCanvas,
-			dummyContext;
 
 		// normalize the value
 		if ( typeof val === 'string' && val.slice(-1) === '%' ) val = (+val.slice(0, -1)) * this.canvas[which] / 100;
 
-		oldHeight = this.canvas.height;
-
 		// get the ratio, in case we're resizing
-		ratio = val / this.canvas[which];
+		ratio = resize ? val / this.canvas[which] : 1;
+
+		// update dimensions of base canvas
+		this.canvas[which] = val;
+		this.context.scale(
+			which === 'width' ? ratio : 1,
+			which === 'height' ? ratio : 1
+		);
 
 		// Update height or width of all the layers' canvases
 		// and update their contexts
 		this.layers.forEach(function(layer, i) {
-
-			imageData = layer.getImageData();
-
-			dummyCanvas = document.createElement('canvas');
-			dummyContext = dummyCanvas.getContext('2d');
-
-			dummyCanvas.setAttribute('width', layer.base.width);
-			dummyCanvas.setAttribute('height', layer.base.height);
-
-			dummyContext.putImageData( imageData, 0, 0 );
 
 			layer.canvas[which] = val;
 
@@ -52,7 +42,7 @@
 				);
 			}
 
-			layer.context.drawImage(dummyCanvas, 0, 0);
+			layer.renderLayer();
 
 		});
 
